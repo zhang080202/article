d46c55a6949180df5ebadadd54975e8f31dc948f
zhangyf <921233440@qq.com>
2018-11-04 22:04:56 +0800

    11

--- src/main/java/com/home/configuration/SwaggerConfiguration.java
+++ src/main/java/com/home/configuration/SwaggerConfiguration.java
@@ -25,9 +25,6 @@ public class SwaggerConfiguration {
 	}
 
 	private ApiInfo apiInfo() {// 接口的相关信息
-		return new ApiInfoBuilder().title("微信小程序 文章分享")
-								   .description("开发文档")
-								   .version("1.0")
-								   .build();
+		return new ApiInfoBuilder().title("微信小程序 文章分享").description("开发文档").version("1.0").build();
 	}
 }
