b84e09b1216dc06bcd8564277b84115e6ed1c031
jiyong <739761380@qq.com>
2018-11-05 16:46:10 +0800

    1

---
+++ src/main/java/com/home/configuration/MybatisPlusConfig.java
@@ -0,0 +1,22 @@
+package com.home.configuration;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+
+@EnableTransactionManagement
+@Configuration
+@MapperScan("com.home.module.*.mapper.mapper*")
+public class MybatisPlusConfig {
+	
+	/**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
